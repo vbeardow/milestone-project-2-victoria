c9d027a935bf552cd4ee96eb5caafedafa83213c
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-20 15:06:09 +0100

    Adjust test_hand tests

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -8,17 +8,12 @@ def my_hand() -> Hand:
     return Hand()
 
 
-@pytest.fixture
-def new_card() -> Card:
-    return Card("Ace", "Hearts")
-
-
 def test_add_card(my_hand: Hand, new_card: Card):
+    new_card = Card("Two", "Hearts")
     expected_hand = my_hand.append(new_card)
     assert my_hand.add_card(new_card) == expected_hand
 
 
-def test_adjust_for_ace(my_hand: Hand, new_card: Card):
-    my_hand.append(new_card)
+def test_adjust_for_ace(my_hand: Hand):
     my_hand.adjust_for_ace()
     assert my_hand.aces == 1
