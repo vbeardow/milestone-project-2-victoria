4783951255458052559a32733b5e1151a9d6a694
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-20 13:42:40 +0100

    Use black formatter

--- methods/deck.py
+++ methods/deck.py
@@ -2,15 +2,15 @@ from typing import List
 from methods.card import Card
 
 
-class Deck():
+class Deck:
     def __init__(self) -> None:
         pass
-    
+
     def __str__(self) -> str:
         pass
-    
+
     def shuffle(self) -> None:
         pass
-    
+
     def deal(self) -> List[Card]:
         pass
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -2,16 +2,22 @@ from methods.deck import *
 import random
 import pytest
 
+
 @pytest.fixture
 def card_deck() -> Deck:
     return Deck()
 
+
 def test_deck_string(card_deck: Deck):
     assert str(card_deck) == "Deck of 52 cards"
-    
+
+
 def test_shuffle(card_deck: Deck):
     shuffled_cards = random.shuffle(card_deck)
-    assert card_deck.shuffle() == shuffled_cards # won't work yet because shuffle will be random. Need to add seed to compare.
+    assert (
+        card_deck.shuffle() == shuffled_cards
+    )  # won't work yet because shuffle will be random. Need to add seed to compare.
+
 
 def test_deal(card_deck: Deck):
     dealt_cards = card_deck.pop(2)
