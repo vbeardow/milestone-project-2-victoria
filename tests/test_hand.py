4c70c36ac43e9cec1cd00a3f76dac63ccc8ace69
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-20 15:21:02 +0100

    Adjust test_hand tests

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -14,6 +14,15 @@ def test_add_card(my_hand: Hand, new_card: Card):
     assert my_hand.add_card(new_card) == expected_hand
 
 
-def test_adjust_for_ace(my_hand: Hand):
-    my_hand.adjust_for_ace()
+def test_add_ace(my_hand: Hand, new_card: Card):
+    new_card = Card("Ace", "Clubs")
+    my_hand.add_card(new_card)
     assert my_hand.aces == 1
+
+
+def test_value_with_ace(my_hand: Hand):
+    my_hand.aces = 1
+    my_hand.value = 21
+    my_hand.adjust_for_ace()
+    assert my_hand.value == 11
+    assert my_hand.aces == 0
