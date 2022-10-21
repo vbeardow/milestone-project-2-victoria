707f0c70169dc9d087ba205384034d7a034b69dc
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-21 15:19:16 +0100

    Add more dosctrings

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -8,12 +8,12 @@ from blackjack.actions import *
 
 
 @pytest.fixture
-def my_hand():
+def my_hand() -> Hand:
     return Hand()
 
 
 @pytest.fixture
-def deck():
+def deck() -> Deck:
     return Deck()
 
 
@@ -31,6 +31,12 @@ def test_twist(my_hand: Hand, deck: Deck):
 
 @patch("builtins.input", lambda *args: "x")
 def test_stick_or_twist_invalid_input(my_hand: Hand, deck: Deck):
+    """Tests exception is raised if invalid input is provided to stick or twist request
+
+    Args:
+        my_hand (Hand): Hand object representing a hand of cards
+        deck (Deck): Deck object representing a deck of cards
+    """
     with pytest.raises(Exception, match="Please enter either s or t"):
         stick_or_twist(my_hand, deck)
 
@@ -38,6 +44,13 @@ def test_stick_or_twist_invalid_input(my_hand: Hand, deck: Deck):
 @patch("builtins.input", lambda *args: "s")
 @patch("builtins.print")
 def test_stick_is_printed(mock_print, my_hand: Hand, deck: Deck):
+    """Test print output if user chooses to stick
+
+    Args:
+        mock_print (_type_): mocking built in print function
+        my_hand (Hand): Hand object representing a hand of cards
+        deck (Deck): Deck object representing a deck of cards
+    """
     stick_or_twist(my_hand, deck)
     mock_print.assert_called_once_with("Player has chosen to stick.")
 
@@ -45,5 +58,12 @@ def test_stick_is_printed(mock_print, my_hand: Hand, deck: Deck):
 @patch("builtins.input", lambda *args: "t")
 @patch("blackjack.actions.twist")
 def test_twist_is_called(twist, my_hand, deck):
+    """Test twist function is called if user chooses to twist
+
+    Args:
+        twist (_type_): mock function of twist action
+        my_hand (Hand): Hand object representing a hand of cards
+        deck (Deck): Deck object representing a deck of cards
+    """
     stick_or_twist(my_hand, deck)
     twist.assert_called_once_with(my_hand, deck)
