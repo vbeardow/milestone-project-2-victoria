d6b3cd37210b30fffd295a75fa6f28c1485c7ec2
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-21 10:24:41 +0100

    Fill out hand class and adjust tests

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -1,11 +1,28 @@
+from blackjack.card import Card
+
+
 class Hand:
-    def __init__(self):
+    """Class representing a hand of cards"""
+
+    def __init__(self) -> None:
+        """Defines the attributes of cards, value and aces. Default is no cards in the hand."""
         self.cards = []
         self.value = 0
         self.aces = 0
 
-    def add_card(self, card):
-        pass
+    def add_card(self, card: Card) -> None:
+        """Adds a single card to hand and adjusts the value and aces count of the hand
 
-    def adjust_for_ace(self):
-        pass
+        Args:
+            card (Card): A single card object
+        """
+        self.cards.append(card)
+        self.value += card.value
+        if card.rank == "Ace":
+            self.aces += 1
+
+    def adjust_for_ace(self) -> None:
+        """Adjusts the value of a hand if it includes aces and the value is above 21"""
+        while self.value > 21 and self.aces > 0:
+            self.value -= 10
+            self.aces -= 1
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -8,14 +8,13 @@ def my_hand() -> Hand:
     return Hand()
 
 
-def test_add_card(my_hand: Hand, new_card: Card):
-    new_card = Card("Two", "Hearts")
-    expected_hand = my_hand.append(new_card)
-    assert my_hand.add_card(new_card) == expected_hand
+def test_add_card(my_hand: Hand):
+    my_hand.add_card(Card("Two", "Hearts"))
+    assert len(my_hand.cards) == 1
     assert my_hand.value == 2
 
 
-def test_add_ace(my_hand: Hand, new_card: Card):
+def test_add_ace(my_hand: Hand):
     new_card = Card("Ace", "Clubs")
     my_hand.add_card(new_card)
     assert my_hand.aces == 1
@@ -23,7 +22,7 @@ def test_add_ace(my_hand: Hand, new_card: Card):
 
 def test_value_with_ace(my_hand: Hand):
     my_hand.aces = 1
-    my_hand.value = 21
+    my_hand.value = 22
     my_hand.adjust_for_ace()
-    assert my_hand.value == 11
+    assert my_hand.value == 12
     assert my_hand.aces == 0
