cd9d94859fcaed23718404918447fa92a1f1b1b7
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-21 10:24:30 +0100

    Fill out deck class and adjust tests

--- blackjack/deck.py
+++ blackjack/deck.py
@@ -1,15 +1,38 @@
 from typing import List
+import random
+from blackjack.constants import *
+from blackjack.card import Card
 
 
 class Deck:
-    def __init__(self) -> None:
-        pass
+    """A class representing a deck of 52 cards"""
 
-    def __str__(self) -> List[str]:
-        pass
+    def __init__(self) -> None:
+        """Constructs the deck of cards with a single card of each suit/rank"""
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(rank, suit))
+
+    def __str__(self) -> str:
+        """Returns a list of cards within the deck
+
+        Returns:
+            List[str]: a list of names of cards within the deck e.g. "Two of Hearts"
+        """
+        card_list = ""
+        for card in self.deck:
+            card_list += (card.__str__()) + "\n"
+        return card_list
 
     def shuffle(self) -> None:
-        pass
+        """Randomly shuffles the deck of cards"""
+        random.shuffle(self.deck)
 
-    def deal(self) -> None:
-        pass
+    def deal(self) -> Card:
+        """Deals a single card off the deck of cards
+
+        Returns:
+            Card: A single card that gets dealt from the deck
+        """
+        return self.deck.pop()
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -4,23 +4,48 @@ from blackjack.deck import Deck
 
 @pytest.fixture
 def card_deck() -> Deck:
+    """Creates a card object
+
+    Returns:
+        Deck: An object representing an ordered deck of cards to test
+    """
     return Deck()
 
 
-def test_deck(card_deck: Deck):
+def test_deck_size(card_deck: Deck):
+    """Tests the number of cards in the deck is 52"""
     assert len(card_deck.deck) == 52
 
 
 def test_deck_string(card_deck: Deck):
-    assert card_deck.__str__()[0] == "Two of Hearts"
+    """Tests the string of first and last cards in an ordered deck
+
+    Args:
+        card_deck (Deck): An ordered deck of 52 cards
+    """
+    card_list = card_deck.__str__().split("\n")[:-1]
+    assert card_list[0] == "Two of Hearts"
+    assert card_list[-1] == "Ace of Clubs"
 
 
 def test_shuffle(card_deck: Deck):
+    """Tests that the order of cards in the deck is not equal to the order of cards after the deck has been shuffled
+
+    Args:
+        card_deck (Deck): An ordered deck of 52 cards
+    """
     ordered_deck = Deck()
     card_deck.shuffle()
     assert ordered_deck.__str__() != card_deck.__str__()
 
 
 def test_deal(card_deck: Deck):
-    dealt_cards = card_deck.deck.pop(2)
-    assert dealt_cards == card_deck.deal()
+    """Tests a single card dealt from the deck
+
+    Args:
+        card_deck (Deck): An ordered deck of 52 cards
+    """
+    new_deck = Deck()
+    expected_cards = new_deck.deck.pop()
+    dealt_cards = card_deck.deal().__str__()
+    assert dealt_cards == expected_cards.__str__()
