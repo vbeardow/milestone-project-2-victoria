07ffdfa16ec7d7b71e1fcdf95d5bc094abbbd5d5
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-10-25 12:14:38 +0100

    Edit too long line

--- blackjack/chips.py
+++ blackjack/chips.py
@@ -5,7 +5,8 @@ class Chips:
     """A class representing a players betting chips"""
 
     def __init__(self):
-        """Set the attributes of chips total to 100 and bet to 0. Do not allow bets higher than the total chips."""
+        """Set the attributes of chips total to 100 and bet to 0.
+        Do not allow bets higher than the total chips."""
         self.total = 100
         self.bet = 0
 
